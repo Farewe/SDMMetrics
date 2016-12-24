1cb2b22a730299b8296042f8562c40fddd67e6c7
Boris Leroy <leroy.boris@gmail.com>
2016-12-24 16:47:24 +0100

    Added new simulations to test the effects of sample size on metrics

--- scripts/simulations.R
+++ scripts/simulations.R
@@ -14,7 +14,8 @@ metrics <- function(x, total)
   results <- data.frame(matrix(nc = 0, nr = nrow(x)))
   
   
-  
+  results$sample.presences <- TP + FN
+  results$sample.absences <- FP + TN
   results$TP <- TP
   results$FP <- FP
   results$FN <- FN
@@ -182,7 +183,7 @@ tiff( './outputs/Figure 2.tiff', width = 450 * 4.2, height = 1000 * 4.2, res = 3
 # png("./outputs/Figure 2.png", h = 800, w = 600)
 ggplot(ggr, aes(x = OPR, y = value, col = prev)) + 
   geom_line(size = .9) + 
-  facet_wrap(~variable, nrow = 3) +
+  facet_wrap(~ variable, nrow = 3) +
   xlab("Simultaneous increase in both over- and underprediction\n(% of actual presences)") + theme_bw() +
   guides(col=guide_legend(title = "Prevalence")) + ylab("Metric value") + 
   theme(legend.position = "top") +
@@ -238,7 +239,7 @@ ggr$value[which(ggr$variable %in% c("a. Jaccard", "c. Unpredicted Presences",
                 ggr$prev != "0.01")] <- NA
 
 
-png("./outputs/Figure S2.2.png", h = 800, w = 600)
+png("./outputs/Figure S2.2.png", h = 600, w = 1000)
 ggplot(ggr, aes(x = OPpc, y = value, col = prev)) + 
   geom_line(size = .9) + 
   facet_wrap(~variable, nrow = 3) +
@@ -304,3 +305,148 @@ ggplot(ggr, aes(x = UTP, y = value, col = prev)) +
   theme(legend.position = "top") +
   coord_fixed(ratio = 100)
 dev.off()
+
+
+# Simulation 6 : presence are fixed, increases in absences
+absences <- seq(50, 9900, 10)
+all.res <- NULL
+res <- data.frame(matrix(nr = 1, nc = 4, dimnames = list(1, c("TP", "FP", "FN", "TN"))))
+for(nbA in absences)
+{
+  res$TP <- 85
+  res$FN <- 15
+  res$FP <- floor(0.15 * nbA)
+  res$TN <- ceiling(0.85 * nbA)
+  all.res <- rbind(all.res, 
+                   res)
+}
+
+results <- metrics(all.res, total = apply(all.res, 1, sum))
+# results <- results[which(results$prevalence <= 0.5), ]
+
+ggr <- melt(results, id.vars = c("prevalence", "pred.prevalence", "TP", "FP", "FN", "TN", "OPR", 
+                                 "UTP", "OPpc", "sample.presences", "sample.absences"), 
+            measure.vars = c("Jaccard", "TSS", "UTP", "Sens", "OPR", "Spe"))
+
+ggr$prevalence <- round(ggr$prevalence, 2)
+ggr$prev <- as.factor(ggr$prevalence)
+ggr$OPR <- ggr$OPR * 100
+ggr$UTP <- ggr$UTP * 100
+
+
+levels(ggr$variable) <- c("a. Jaccard", "b. True Skill Statistic", "c. Unpredicted Presences", 
+                          "d. Sensitivity", "e. OverPrediction Rate", "f. Specificity")
+
+
+png("./outputs/Figure S2.3.png", h = 800, w = 600)
+ggplot(ggr, aes(x = sample.absences, y = value)) + 
+  geom_line(size = .9) + 
+  facet_wrap(~variable, nrow = 3) +
+  xlab("Number of absences\n(Number of presences = 100)") + theme_bw() +
+  guides(col=guide_legend(title = "Prevalence")) + ylab("Metric value") + 
+  theme(legend.position = "top") #+
+  # coord_fixed(ratio = 100)
+dev.off()
+
+# Simulation 7 : comparing metrics for a good and a bad model, with different sample sizes
+presences <- rep(100, 3)
+absences <- c(100, 1000, 10000)
+true.prev <- c(.01, .10, .5)
+area <- 100000
+true.presences <- true.prev * area
+true.absences <- area - (true.prev * area)
+models <- list(M1 = list(TP = .85,
+                         FP = .15,
+                         FN = .15),
+               M2 = list(TP = .60,
+                         FP = .40,
+                         FN = .40),
+               M3 = list(TP = .80,
+                         FP = 0,
+                         FN = .20),
+               M4 = list(TP = .80,
+                         FP = .20,
+                         FN = 0))
+
+all.res <- NULL
+
+sim <- 10
+
+res <- data.frame(matrix(nr = 1, nc = 6, dimnames = list(1, c("model", "sp.prev", "TP", "FP", "FN", "TN"))))
+for(i in 1:sim)
+{
+  for(nbP in presences)
+  {
+    for(nbA in absences)
+    {
+      for(md in 1:length(models))
+      {
+        for(sp.prev in 1:length(true.prev))
+        {
+          pred.pres <- c(rep(1, models[[md]]$TP * true.presences[sp.prev]),
+                         rep(0, true.presences[sp.prev] - models[[md]]$TP * true.presences[sp.prev]))
+          pred.abs <- c(rep(1, length(pred.pres) * models[[md]]$FP),
+                        rep(0, true.absences[sp.prev] - length(pred.pres) * models[[md]]$FP))
+          obs.pres <- sample(pred.pres, nbP)
+          obs.abs <- sample(pred.abs, nbA)
+          
+          res$model <- md
+          res$sp.prev <- true.prev[sp.prev]
+          
+          res$TP <- length(which(obs.pres == 1))
+          res$FN <- length(which(obs.pres == 0))
+          res$FP <- length(which(obs.abs == 1))
+          res$TN <- length(which(obs.abs == 0))
+          all.res <- rbind(all.res, 
+                           res)
+        }      
+      }
+    }
+  }
+  
+}
+
+results <- metrics(all.res, total = apply(all.res[, 3:6], 1, sum))
+results$model <- all.res$model
+results$sp.prev <- all.res$sp.prev
+results$c.val <- results$sample.presences / ((results$sample.absences + results$sample.presences) * results$sp.prev)
+results$Fcpb <- 2 * results$TP / (results$TP + results$FN + results$FP * results$c.val)
+# results <- results[which(results$prevalence <= 0.5), ]
+
+ggr <- melt(results, id.vars = c("prevalence", "pred.prevalence", "TP", "FP", "FN", "TN", "OPR", 
+                                 "UTP", "OPpc", "sample.presences", "sample.absences", "model", "sp.prev"), 
+            measure.vars = c("Jaccard", "Fcpb", "TSS"))
+
+ggr$prevalence <- round(ggr$prevalence, 2)
+ggr$prev <- as.factor(ggr$prevalence)
+ggr$OPR <- ggr$OPR * 100
+ggr$UTP <- ggr$UTP * 100
+
+ggr$sample.size <- ggr$sample.absences + ggr$sample.presences
+ggr$model <- as.factor(ggr$model)
+ggr$sample.absences <- as.factor(ggr$sample.absences)
+levels(ggr$model) <- c("OPR = UP = 0.15", "OPR = UP = 0.40", "OPR = 0, UP = 0.20", "OPR = 0.20, UP = 0")
+
+levels(ggr$variable) <- c("a. Jaccard", "b. Fcpb", "c. True Skill Statistic")
+
+
+# png("./outputs/Figure S2.3.png", h = 800, w = 600)
+ggplot(ggr, aes(x = sp.prev, y = value, col = model)) + 
+  geom_point(aes(shape = sample.absences)) + 
+  stat_smooth(se = T) +
+  facet_wrap(~variable, nrow = 1) +
+  # xlab("Number of absences\n(Number of presences = 100)") +
+  theme_bw() +
+  # guides(col=guide_legend(title = "Prevalence")) + ylab("Metric value") + 
+  theme(legend.position = "top") #+
+# coord_fixed(ratio = 100)
+# dev.off()
+
+# Clearly, the results indicate that none of the metrics can provide
+# a value independent of prevalence.
+# The problem is most likely linked to the sampling of absences which
+# is dependent on species prevalence. For TSS, low prevalences mean
+# high numbers of TN and thus high values of sensitivity --> high TSS.
+# For Jaccard, low prevalences mean low likelihood of sampling 
+# FP, and thus higher values. Conversely, high prevalences results
+# in higher likelihood of sampling FPs, and thus lower values. 
