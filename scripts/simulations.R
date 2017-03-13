34a4c0b968b5df5918ca325267ecefe5153a05f7
Boris Leroy <leroy.boris@gmail.com>
2017-03-13 11:24:35 +0100

    New metrics introduced

--- scripts/simulations.R
+++ scripts/simulations.R
@@ -408,10 +408,15 @@ for(i in 1:sim)
 results <- metrics(all.res, total = apply(all.res[, 3:6], 1, sum))
 results$model <- all.res$model
 results$sp.prev <- all.res$sp.prev
-results$c.val <- results$sample.presences * (1 - results$sp.prev) / (results$sample.absences * results$sp.prev)
-results$OPRp <- results$FP * results$c.val / (results$TP + results$FP * results$c.val)
-results$Jacp <- results$TP / (results$TP + results$FN + results$FP * results$c.val)
+results$x.val <- results$sample.presences * (1 - results$sp.prev) / (results$sample.absences * results$sp.prev)
+results$OPRp <- results$FP * results$x.val / (results$TP + results$FP * results$x.val)
+results$Jacp <- results$TP / (results$TP + results$FN + results$FP * results$x.val)
 # results <- results[which(results$prevalence <= 0.5), ]
+
+# Fcpb by Li & Guo
+results$c.val <- results$sample.presences / (results$sp.prev * results$sample.absences)
+results$Fcpb <- results$TP / (results$TP + results$FN + results$FP * results$c.val)
+
 
 # Number of FP in the sample for each prevalence
 mean(results$OPRp[which(results$sample.absences == 500 & results$model == 1 & results$sp.prev == 0.01)])
@@ -420,7 +425,7 @@ mean(results$OPRp[which(results$sample.absences == 500 & results$model == 1 & re
 
 ggr <- melt(results, id.vars = c("prevalence", "pred.prevalence", "TP", "FP", "FN", "TN", "OPR", 
                                  "UTP", "OPpc", "sample.presences", "sample.absences", "model", "sp.prev", "OPRp"), 
-            measure.vars = c("TSS", "Jaccard", "Jacp"))
+            measure.vars = c("TSS", "Jaccard", "Fcpb","Jacp"))
 
 ggr$prevalence <- round(ggr$prevalence, 2)
 ggr$prev <- as.factor(ggr$prevalence)
@@ -434,22 +439,21 @@ levels(ggr$model) <- c("OPR = UP = 0.40", "OPR = 0, UP = 0.40", "OPR = 0.40, UP 
 levels(ggr$sample.absences) <- c("500 absence points\nSample\nprevalence = 0.50", 
                                  "1000 absence points\nSample\nprevalence = 0.33", 
                                  "10 000 absence points\nSample\nprevalence = 0.05")
-levels(ggr$variable) <- c("a. True Skill Statistic", "b. Jaccard", "c. Prevalence calibrated\nJaccard")
+levels(ggr$variable) <- c("a. True Skill Statistic", "b. Jaccard", "c. Fcpb", "d. Prevalence calibrated\nJaccard")
 
 
 
-# png("./outputs/Figure S2.3.png", h = 800, w = 600)
-ggplot(ggr, aes(x = sp.prev, y = value, col = model)) + 
-  geom_point() + 
+png("./outputs/Figure presence-absence.png", h = 800, w = 960)
+ggplot(ggr, aes(x = sp.prev, y = value, col = model)) +
+  geom_point() +
   stat_smooth(se = T) +
   facet_grid(sample.absences~variable) +
   xlab("Species prevalence") +
   theme_bw() +
-  guides(col=guide_legend(title = "Models")) + ylab("Metric value") + 
-  theme(legend.position = "top") + 
-  geom_hline(yintercept = c(6/14, 6/10))#+
-# coord_fixed(ratio = 100)
-# dev.off()
+  guides(col=guide_legend(title = "Models")) + ylab("Metric value") +
+  theme(legend.position = "top") +
+  geom_hline(yintercept = c(6/14, 6/10), linetype = 2)
+dev.off()
 
 
 
@@ -536,7 +540,7 @@ results$Fcpb <- results$TP / (results$TP + results$FN + results$FP * results$c.v
 results$FFP <- (1 - results$UTP) * (results$sp.prev * area - results$sample.presences)
 results$FPprim <- (results$FP / results$sample.absences) * (area - results$sample.presences) - results$FFP
 results$FPprim <- results$sample.absences * (results$FPprim / (area * (1 - results$sp.prev)))
-results$FPprim <- sapply(results$FPprim, FUN = max, 0)
+# results$FPprim <- sapply(results$FPprim, FUN = max, 0)
 results$OPRppa <- results$FPprim * results$x.val / (results$TP + results$FPprim * results$x.val)
 results$Jacppa <- results$TP / (results$TP + results$FN + results$FPprim * results$x.val)
 
@@ -572,7 +576,107 @@ ggplot(ggr, aes(x = sp.prev, y = value, col = model)) +
   theme_bw() +
   guides(col=guide_legend(title = "Models")) + ylab("Metric value") + 
   theme(legend.position = "top") + 
-  geom_hline(yintercept = c(6/14, 6/10))
+  geom_hline(yintercept = c(6/14, 6/10), linetype = 2)
 dev.off()
 
+
+
+# Simulation 9 :  ideal case sample = species prevalence
+true.prev <- c(.01, .10, .5)
+area <- 100000
+true.presences <- true.prev * area
+true.absences <- area - (true.prev * area)
+models <- list(M1 = list(TP = .60, 
+                         OPR = .40,
+                         UP = .40), # HAS TO BE 1 - TP
+               M2 = list(TP = .60,
+                         OPR = 0,
+                         UP = .40),
+               M3 = list(TP = 1,
+                         OPR = .40,
+                         UP = 0))
+
+all.res <- NULL
+
+sim <- 5
+
+res <- data.frame(matrix(nr = 1, nc = 6, dimnames = list(1, c("model", "sp.prev", "TP", "FP", "FN", "TN"))))
+for(i in 1:sim)
+{
+  for(md in 1:length(models))
+  {
+    for(sp.prev in 1:length(true.prev))
+    {
+      # pred.obs.pres : Predictions for the species distribution range only
+      pred.obs.pres <- c(rep(1, models[[md]]$TP * true.presences[sp.prev]),
+                         rep(0, true.presences[sp.prev] - models[[md]]$TP * true.presences[sp.prev]))
+      # pred.obs.abs : Predictions for areas  where the species is absent 
+      pred.obs.abs <- c(rep(1, models[[md]]$OPR * models[[md]]$TP * true.presences[sp.prev] / (1 - models[[md]]$OPR)),
+                        rep(0, true.absences[sp.prev] - 
+                              models[[md]]$OPR * models[[md]]$TP * true.presences[sp.prev] / (1 - models[[md]]$OPR)))
+      
+      nbP <- 500
+      nbA <- nbP * (1 - true.prev[sp.prev]) / true.prev[sp.prev]
+      
+      sample.pres <- sample(pred.obs.pres, nbP)
+      sample.abs <- sample(pred.obs.abs, nbA)
+      
+      
+      res$model <- md
+      res$sp.prev <- true.prev[sp.prev]
+      
+      res$TP <- length(which(sample.pres == 1))
+      res$FN <- length(which(sample.pres == 0))
+      res$FP <- length(which(sample.abs == 1))
+      res$TN <- length(which(sample.abs == 0))
+      all.res <- rbind(all.res, 
+                       res)
+    }      
+  }
+  
+}
+
+results <- metrics(all.res, total = apply(all.res[, 3:6], 1, sum))
+results$model <- all.res$model
+results$sp.prev <- all.res$sp.prev
+results$x.val <- results$sample.presences * (1 - results$sp.prev) / (results$sample.absences * results$sp.prev)
+results$OPRp <- results$FP * results$x.val / (results$TP + results$FP * results$x.val)
+results$Jacp <- results$TP / (results$TP + results$FN + results$FP * results$x.val)
+# results <- results[which(results$prevalence <= 0.5), ]
+
+# Fcpb by Li & Guo
+results$c.val <- results$sample.presences / (results$sp.prev * results$sample.absences)
+results$Fcpb <- results$TP / (results$TP + results$FN + results$FP * results$c.val)
+
+
+
+ggr <- melt(results, id.vars = c("prevalence", "pred.prevalence", "TP", "FP", "FN", "TN", "OPR", 
+                                 "UTP", "OPpc", "sample.presences", "sample.absences", "model", "sp.prev", "OPRp"), 
+            measure.vars = c("TSS", "Jaccard"))
+
+ggr$prevalence <- round(ggr$prevalence, 2)
+ggr$prev <- as.factor(ggr$prevalence)
+ggr$OPR <- ggr$OPR * 100
+ggr$UTP <- ggr$UTP * 100
+
+ggr$sample.size <- ggr$sample.absences + ggr$sample.presences
+ggr$model <- as.factor(ggr$model)
+ggr$sample.absences <- as.factor(ggr$sample.absences)
+levels(ggr$model) <- c("OPR = UP = 0.40", "OPR = 0, UP = 0.40", "OPR = 0.40, UP = 0")
+
+levels(ggr$variable) <- c("a. True Skill Statistic", "b. Jaccard")
+
+
+
+png("./outputs/Figure sample = species prevalence.png", h = 800, w = 960)
+ggplot(ggr, aes(x = sp.prev, y = value, col = model)) +
+  geom_point() +
+  stat_smooth() +
+  facet_grid(~variable) +
+  xlab("Species prevalence") +
+  theme_bw() +
+  guides(col=guide_legend(title = "Models")) + ylab("Metric value") +
+  theme(legend.position = "top") +
+  geom_hline(yintercept = c(6/14, 6/10), linetype = 2)
+dev.off()
 
