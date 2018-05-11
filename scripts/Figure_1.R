4141e24694466086138c900e206cf5ef263544fb
Farewe <leroy.boris@gmail.com>
2018-05-11 11:23:51 +0200

    Cleaning

--- scripts/Figure_1.R
+++ scripts/Figure_1.R
@@ -1,23 +1,23 @@
 
-#### Choisir la pr?valence pour les 3 derni?res figures ####
+#### Defining confusion matrices for figure 1 ####
 #        Fig.1  Fig.2  Fig.3          
-FN =  c(     0,     0,    15); 
+FN =  c(     0,    0,    15)
 TP =  c(   100,   85,    85)
-FP =  c(    300,   15,     0)
+FP =  c(   300,   15,     0)
 
-#Pr?valence :  Fig.4  Fig.5  Fig.6 
+#Prevalence for  Fig.4  Fig.5  Fig.6 
 prevalence = c(  0.25,  0.1,   0.01 )
 
-#OPR & UTP ? fixer :
-OPR = 0.3
-UTP = 0.3
+# Equal amounts of overprediction and underprediction for fig 4/5/6
+overpred = 0.3
+underpred = 0.3
 
 total = 10000
 obs  = TP+FN
 obs  = c(obs, round( prevalence * total ))
-FN   = c(FN,  round( prevalence * total * UTP ))
-TP   = c(TP,  round( prevalence * total * (1-UTP) ))
-FP   = c(FP,  round( TP[4:6] * (OPR/(1-OPR)) ))
+FN   = c(FN,  round( prevalence * total * underpred ))
+TP   = c(TP,  round( prevalence * total * (1-underpred) ))
+FP   = c(FP,  round( TP[4:6] * (overpred/(1-overpred)) ))
 pred = TP+FP
 TN   = total-obs-FP
 
@@ -25,7 +25,7 @@ TN   = total-obs-FP
 
 
 
-#### Calcul des m?triques ####
+#### Metric calculation ####
 Jaccard  = round(   TP / (  TP+FN+FP)  ,2) 
 Sorensen = round( 2*TP / (2*TP+FN+FP)  ,2)
 OPR  = round( FP / pred ,2)
@@ -34,7 +34,6 @@ Sens = round( TP / (TP+FN) ,2)
 Spe  = round( TN / (TN+FP) ,2)
 TSS  = round( Sens + Spe - 1 ,2)
    x = TP-pred*obs/total
-# TSS   = round( x / (x +FP*(obs/total) +FN*(1-obs/total))  ,2)
 Kappa = round( x / (x +FP*(1/2)       +FN*(1/2)        )  ,2) 
 Prevalence = round( (TP+FN) / total  ,2)
 
@@ -42,7 +41,7 @@ df <- data.frame(TP, TN, FP, FN, obs, pred, OPR, UTP,Jaccard, Sorensen, Sens, Sp
 print(df)
 
 
-#### Param?tres graphiques ####
+#### Graphical parameters ####
 width  = 1200 #pixels
 height = 675  #pixels
 row = 2
@@ -56,13 +55,12 @@ titles=c(paste0(FP[1]/TP[1] * 100, '% overprediction,\n0% under-prediction,\npre
          paste('30% over- & under-prediction,\nprevalence = ' , df$Prevalence[6],sep=""))
 
 
-#### Param?tres d'affichage des cercles ####
+#### Circle parameters ####
 area=height*width
 diametres.obs <-  2*sqrt(obs*area /(total*pi))/96 #inches
 diametres.pred <- 2*sqrt(pred*area/(total*pi))/96 #inches
 ratio = df$UTP
 
-# png( 'C:/Users/BorisMNHN/Google Drive/recherche/publis/21 - Metriques virtual species/Figure 1.png', width = width, height = height )  
 png( './outputs/Figure 1.png', width = width * 4.2, height = height * 4.2, res = 300)  
 par( mfrow = c(row,col), mar = c(0,0,0,0) )
 #### Plots ####
@@ -82,13 +80,12 @@ for (i in 1:6)
     x1=ratio[i]*(diametres.pred[i]+diametres.obs[i])+x0}
   
   
-# Aire Observ?e
+# Observed range
 symbols(  x0,y0, circles=diametres.obs[i],  ylim=c(-10,10), xlim=c(-10,10), inches = FALSE,  xaxt='n', yaxt='n', ann=FALSE, bg=gray(0.1, alpha = .5))
-#Aire Pr?dite
+# Predicted range
 symbols(  x1,y0, fg = grey(.3), circles=diametres.pred[i], ylim=c(-10,10), xlim=c(-10,10), inches = FALSE,  xaxt='n', yaxt='n', ann=FALSE, bg=gray(0.85, alpha = .5), add=T)
 
 
-# Titre et affichage des m?triques
 
 if(i <= 3)
 {
@@ -101,18 +98,13 @@ if(i <= 3)
 }
 mtext(paste(letters[i], lett, sep=""), adj=0.03, side=1, line=-1.5, cex=1.6, font=1)
 mtext(titles[i], adj=.5, side=1, line=-1.5, cex=1.6, font=1)
-# mtext(paste('TN =',   TN[i], '\nTP =',       TP[i], '\nFN =',            FN[i], '\nFP =',             FP[i], sep=" "), adj=0.02, side=3, line=-7, cex=1)
+
 mtext(paste('TSS = ', format(TSS[i], nsmall = 2),
             '\nSensitivity = ', format(Sens[i], nsmall = 2),
             '\nSpecificity = ',   format(Spe[i], nsmall = 2), sep = ""), adj=0.05, side=3, line=-7, cex=1.6)
-mtext(paste(#'\nJaccard = ',  format(Jaccard[i], nsmall = 2),
-            '\nS\u00F8rensen = ',  format(Sorensen[i], nsmall = 2),
+mtext(paste('\nS\u00F8rensen = ',  format(Sorensen[i], nsmall = 2),
             '\nUPR = ', format(UTP[i], nsmall = 2), 
             '\nOPR = ', format(OPR[i], nsmall = 2), sep=""), adj=0.95, side=3, line=-7, cex=1.6)
-# mtext(paste('UTP = ', format(UTP[i], nsmall = 2), 
-#             '\nOPR = ',     format(OPR[i], nsmall = 2), 
-#             '\nSensitivity = ', 
-#             '\nSpecificity = ',   format(Spe[i], nsmall = 2), sep=""), adj=0.95, side=1, line=-2, cex=1)
 }
 
 dev.off()
